8a36df1b087140e5aa4b2685a2bf01b11f41b0c4
davidgomez0429 <gomezd6@mcmaster.ca>
2020-07-25 13:14:58 -0400

    Updates

---
+++ precache-manifest.a26e5c67dcf744ca624a88c2140cbabd.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "349efb8eef7c7bfad5c0d80335137049",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "f227cb20c31b0709ddb2",
+    "url": "/website/static/css/2.3d20797f.chunk.css"
+  },
+  {
+    "revision": "c85e50c796629c518a71",
+    "url": "/website/static/css/main.dd8a6d06.chunk.css"
+  },
+  {
+    "revision": "f227cb20c31b0709ddb2",
+    "url": "/website/static/js/2.5180df36.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/website/static/js/2.5180df36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c85e50c796629c518a71",
+    "url": "/website/static/js/main.b4132bea.chunk.js"
+  },
+  {
+    "revision": "6cf7fc8c51c9078ec993",
+    "url": "/website/static/js/runtime-main.279f26b3.js"
+  },
+  {
+    "revision": "b378aaaa9dd883aa0517443aaa6eea95",
+    "url": "/website/static/media/1.b378aaaa.png"
+  },
+  {
+    "revision": "1db7c177513b425290506aaf3b7edc3f",
+    "url": "/website/static/media/about.1db7c177.png"
+  },
+  {
+    "revision": "e34f255bdb9eb818c3f7331e7b8944a3",
+    "url": "/website/static/media/amazon_logo.e34f255b.jpg"
+  },
+  {
+    "revision": "f9426f8cde97a651b2d21e611b359db6",
+    "url": "/website/static/media/banner.f9426f8c.png"
+  },
+  {
+    "revision": "4a16e4c5e059250e43b579e60214aab9",
+    "url": "/website/static/media/banner2.4a16e4c5.png"
+  },
+  {
+    "revision": "eaf70cec0eb57a4c532be7b13a30273f",
+    "url": "/website/static/media/bradcam.eaf70cec.png"
+  },
+  {
+    "revision": "d10d5ea78317767a975d3089701d4e19",
+    "url": "/website/static/media/bradcam_2.d10d5ea7.png"
+  },
+  {
+    "revision": "e91a2730438fad56f469cfad63cde4a9",
+    "url": "/website/static/media/bradcam_3.e91a2730.png"
+  },
+  {
+    "revision": "f10a3037a8cda3791e2116018033b8c6",
+    "url": "/website/static/media/facebook_logo.f10a3037.png"
+  },
+  {
+    "revision": "34715858cff3404e0cfc1e4fd2478829",
+    "url": "/website/static/media/logo.34715858.jpg"
+  },
+  {
+    "revision": "6a35931133f1703c447826ba8a88c940",
+    "url": "/website/static/media/mcdonalds_logo.6a359311.png"
+  },
+  {
+    "revision": "a12bd7437f32c9f3c8951ccc39a0fb89",
+    "url": "/website/static/media/microsoft_logo.a12bd743.jpg"
+  },
+  {
+    "revision": "a01de9e55f86e05077d52f82b28d3943",
+    "url": "/website/static/media/product.a01de9e5.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/website/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/website/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/website/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "cd82779b3e397f6dbcc7e2bb81784a2e",
+    "url": "/website/static/media/walmart_logo.cd82779b.jfif"
+  },
+  {
+    "revision": "9369ca151502af116458373d2e0ab6ba",
+    "url": "/website/static/media/work.9369ca15.png"
+  }
+]);
